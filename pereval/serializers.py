24c65bfe0da629a136b9bd4bbdbbefb55f49391c
Baradavid <baramidze.david12@gmail.com>
2023-05-18 17:41:26 +0400

    Добавил сериализаторы

---
+++ pereval/serializers.py
@@ -0,0 +1,32 @@
+from .models import Users, PerevalAdded, Coords, PerevalAreas, PerevalImages
+from rest_framework.serializers import ModelSerializer
+
+
+class UsersSerializer(ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
+
+
+class CoordsSerializer(ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = '__all__'
+
+
+class PerevalAddedSerializer(ModelSerializer):
+    class Meta:
+        model = PerevalAdded
+        fields = '__all__'
+
+
+class PerevalImagesSerializer(ModelSerializer):
+    class Meta:
+        model = PerevalImages
+        fields = '__all__'
+
+
+class PerevalAreasSerializer(ModelSerializer):
+    class Meta:
+        model = PerevalAreas
+        fields = '__all__'
