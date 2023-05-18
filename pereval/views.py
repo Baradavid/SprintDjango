01f776379974f595668ceebfa5d6a0e3053953cc
Baradavid <baramidze.david12@gmail.com>
2023-05-18 18:00:54 +0400

    Добавил вью для сериализаторов

--- pereval/views.py
+++ pereval/views.py
@@ -1,2 +1,20 @@
+import django_filters
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
+from .filters import PerevalFilter
+from .models import PerevalAdded
+from .serializers import PerevalAddedSerializer
+
+
+class PerevalViewSet(ModelViewSet):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalAddedSerializer
+
+
+class PerevalList(ListAPIView):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalAddedSerializer
+    filterset_class = PerevalFilter
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
