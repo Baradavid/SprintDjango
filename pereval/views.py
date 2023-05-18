a7a885097c03b221a277d97433db9aec0c63659a
Baradavid <baramidze.david12@gmail.com>
2023-05-18 18:40:57 +0400

    Добавил метод 'POST submitData'

--- pereval/views.py
+++ pereval/views.py
@@ -1,6 +1,8 @@
 import django_filters
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import PerevalFilter
@@ -18,3 +20,13 @@ class PerevalList(ListAPIView):
     serializer_class = PerevalAddedSerializer
     filterset_class = PerevalFilter
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+
+
+@api_view(['POST'])
+def submitData(request):
+    serializer = PerevalAddedSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
