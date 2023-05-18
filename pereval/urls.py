3dca8a73c118473d6df2bf62534405b2bf5a9330
Baradavid <baramidze.david12@gmail.com>
2023-05-18 19:56:00 +0400

    Добавил урл по созданию объекта

--- pereval/urls.py
+++ pereval/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import PerevalList
+from .views import PerevalList, submitData
 
 urlpatterns = [
     path('submitData/', PerevalList.as_view(), name='pereval_list'),
+    path('submitData/create/', submitData, name='submitData')
     ]
-
