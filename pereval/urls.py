cd9afde19f0f4c82d014a3464417b72b578ed206
Baradavid <baramidze.david12@gmail.com>
2023-05-24 16:15:12 +0400

    Отредактировал урлы для всех методов

--- pereval/urls.py
+++ pereval/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PerevalList, submitData
+from .views import PerevalList, submitData, getData, update_pereval
 
 urlpatterns = [
     path('submitData/', PerevalList.as_view(), name='pereval_list'),
-    path('submitData/create/', submitData, name='submitData')
+    path('submitData/create/', submitData, name='submitData'),
+    path('submitData/<int:pk>/', getData, name='get_data'),
+    path('submitData/<int:pk>/update', update_pereval, name='update_pereval'),
     ]
