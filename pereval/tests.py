371b2b2809c1694e949bffc78bdea409d362ce51
Baradavid <baramidze.david12@gmail.com>
2023-05-30 18:20:07 +0400

    Начал работу над тестами для приложения

--- pereval/tests.py
+++ pereval/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import Users, PerevalAdded, Coords, PerevalImages
+
+
+class PerevalTests(APITestCase):
+    def setUp(self):
+        self.url_post = reverse('submitData')
+        self.url_post = reverse('getData', kwargs={'pk': 2})
+        self.user = Users.objects.create(first_name='TestUser', last_name='Test',
+                                         password='testpass', email='Testemail')
+        self.coord = Coords.objects.create(latitude=1.0, longitude=3.0, height=2)
+        self.img = PerevalImages.objects.create(image='Testimage.jpg',
+                                                image_name='Testimage')
+        self.valid_payload = {
+            'user': self.user.pk,
+            'beauty_title': 'Test Beauty Title',
+            'title': 'Test Title',
+            'other_titles': 'Test Other Titles',
+            'connect': 'Test Connect',
+            'winter_level': 'Test Winter Level',
+            'spring_level': 'Test Spring Level',
+            'summer_level': 'Test Summer Level',
+            'autumn_level': 'Test Autumn Level',
+            'coord_id': self.coord.pk,
+            'images': [self.img.pk],
+            'status': 'new'
+        }
+
+
+
+
